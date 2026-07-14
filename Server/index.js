1e176186d095f89af560cacbd22f5fcb837a390f
Samresh8152 <samreshc31@gmail.com>
2026-07-14 17:36:05 +0530

    auth controller

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import ConnectDB from "./Configs/ConnectDB.js";
+import cookieParser from "cookie-parser";
+import authRouter from "./Routes/auth.routes.js";
 
 dotenv.config();
 
 const app =express();
 
-const PORT = process.env.PORT;
+app.use(express.json());
+// app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+
 
 app.get("/",(req,res)=>{
     res.json("hello");
 })
+const PORT = process.env.PORT;
+app.use("/api/auth",authRouter);
+
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
     ConnectDB();
